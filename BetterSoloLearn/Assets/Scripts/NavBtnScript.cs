b0f8608dc8d78b6996a1332aa9af1df7e64113e9
inertjohnjunk <backstabber111@hotmail.com>
2021-05-31 11:13:12 +0200

    Tagged scene elements, Navbuttons functionality WIP

--- BetterSoloLearn/Assets/Scripts/NavBtnScript.cs
+++ BetterSoloLearn/Assets/Scripts/NavBtnScript.cs
@@ -1,25 +1,67 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UIElements;
 
 public class NavBtnScript : MonoBehaviour
 {
-    public static NavBtnScript instance;
-
-    private void Awake()
+    public void Navigate()
     {
-        if (instance == null)
+        Debug.Log(this.tag);
+        Debug.Log("Running navigate script");
+        if (this.tag == "NavToLoginBtn")
         {
-            instance = this;
+            Debug.Log("Button pressed is Login button");
+            if (GameObject.FindWithTag("AuthDisplay"))
+            {
+                GameObject.FindWithTag("AuthDisplay").SetActive(true);
+                Debug.Log("Found AuthDisplay");
+            }
+            if (GameObject.FindWithTag("LogInUI"))
+            {
+                GameObject.FindWithTag("LogInUI").SetActive(true);
+                Debug.Log("Found LogInUI");
+            }
+            if (GameObject.FindWithTag("SignInUI"))
+            {
+                GameObject.FindWithTag("SignInUI").SetActive(false);
+                Debug.Log("Found SignInUI");
+            }
+            if (GameObject.FindWithTag("MainDisplay"))
+            {
+                GameObject.FindWithTag("MainDisplay").SetActive(false);
+                Debug.Log("Found MainDisplay");
+            }
         }
-        else if (instance != null)
+        else
         {
-            Debug.Log("Instance already exists, destroying object!");
-            Destroy(this);
+            Debug.Log("did not run Login button code");
         }
-    }
-    private void Navigate()
-    { 
-    
+        if (this.tag == "NavToSigninBtn")
+        {
+            Debug.Log("Button pressed is Signin button");
+            if (gameObject.tag == "AuthDisplay")
+            {
+                gameObject.SetActive(true);
+            }
+            if (gameObject.tag == "SignInUI")
+            {
+                gameObject.SetActive(true);
+            }
+            if (gameObject.tag == "LogInUI")
+            {
+                gameObject.SetActive(false);
+            }
+            if (gameObject.tag == "MainDisplay")
+            {
+                gameObject.SetActive(false);
+            }
+        }
+        else
+        {
+            Debug.Log("Did not run Signin button code");
+        }
+
     }
 }
