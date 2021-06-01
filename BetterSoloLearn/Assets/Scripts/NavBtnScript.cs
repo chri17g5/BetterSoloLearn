4b59f4a26ffa9052db8045892e80cd58c1726ebf
inertjohnjunk <backstabber111@hotmail.com>
2021-06-01 09:49:20 +0200

    Added comments to to NavBtnScript

--- BetterSoloLearn/Assets/Scripts/NavBtnScript.cs
+++ BetterSoloLearn/Assets/Scripts/NavBtnScript.cs
@@ -7,6 +7,9 @@ using UnityEngine.UIElements;
 public class NavBtnScript : MonoBehaviour
 {
     public static NavBtnScript instance;
+
+    //Assign GameObjects with variable names,
+    //so that they can be called in the script later, whether active in Unity or not.
     GameObject AD;
     GameObject MD;
     GameObject LIUI;
@@ -15,6 +18,7 @@ public class NavBtnScript : MonoBehaviour
     GameObject NTSB;
     private void Awake()
     {
+        //Assign continued.
         LIUI = GameObject.FindWithTag("LogInUI");
         SIUI = GameObject.FindWithTag("SignInUI");
         AD = GameObject.FindWithTag("AuthDisplay");
@@ -22,16 +26,28 @@ public class NavBtnScript : MonoBehaviour
     }
     private void Start()
     {
+        //Deactivates AuthDisplay as it is not supposed to be active on startup.
+        //At least not in this iteration of the app.
         AD.SetActive(false);
     }
 
     public void Navigate()
     {
+        //Every time button/Navigate() is called, a new instance is declared,
+        //making sure the specific code that runs is the correct one according to its tag.
         instance = this;
-        Debug.Log($"This tag is {this.tag}" + $" And instance tag is {instance.tag}");
+
+        //Logs both this.tag and instance.tag as they need to be the same
+        //so that the if statement does not receive incorrect information.
+        //Used this to notice a bug as instance.tag was assigned only once, on awake,
+        //so no matter which button was pressed it ran only one cluster of code
+        Debug.Log($"This tag is {this.tag} And instance tag is {instance.tag}");
+
+        //Notifies that the Navigate() method actually runs when intended.
         Debug.Log("Running navigate script");
         if (instance.tag == "NavToLoginBtn")
         {
+            //Debug.Logs to make sure that the correct things are happening in the correct order
             Debug.Log("Button pressed is Login button");
 
                 AD.SetActive(true);
@@ -50,17 +66,26 @@ public class NavBtnScript : MonoBehaviour
         }
         else
         {
+            //Extra precaution to check whether it ran any of the two if statements.
+            //The Signin button has the same else.
             Debug.Log("did not run Login button code");
         }
         if (instance.tag == "NavToSigninBtn")
         {
             if (instance.tag == "NavToLoginBtn")
             {
+                //Checks to see if the instance tag (SOMEHOW, No idea how exactly) is truly "NavToSigninBtn"
+                //Never actually ran, which was in itself instrumental in pointing my attention elsewhere,
+                //to the real problem.(See Line 36)
                 Debug.Log("Wr0ng wr0ng wr0ng!");
+
+                //Note: I was losing my patience at this point. Hence the unprofessional Debug.Log.
+
+
                 return;
             }
             Debug.Log("Button pressed is SignIn button");
-            Debug.Log($"This tag is {this.tag}" + $" And instance tag is {instance.tag}");
+            Debug.Log($"This tag is {this.tag} And instance tag is {instance.tag}");
 
             AD.SetActive(true);
              Debug.Log("Activating AuthDisplay");
